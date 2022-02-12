5875e0417857d36e3de4386e8b9001066d2ce7b5
miferco97 <miferco97@gmail.com>
2022-02-12 22:09:00 +0100

    bug solved

--- core_setup.bash
+++ core_setup.bash
@@ -3,12 +3,12 @@
 #install curl and tmux
 sudo apt install curl tmux -y
 # install zsh
-sudo apt install zsh -y
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-zsh -c 'print zsh installed correctly :D; zsh -i' && \
-sudo apt-get install fonts-powerline
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
-git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+sudo apt install zsh -y && \ 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && \ 
+sudo apt-get install fonts-powerline -y && \
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k && \
+git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions && \
+zsh -c 'print zsh installed correctly :D; zsh -i' 
 
 # install nvim
 pushd $HOME/.dotfiles/nvim/.config/nvim/
@@ -23,3 +23,4 @@ sudo dpkg -i ./keyring.deb
 sudo echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list
 sudo apt update
 sudo apt install i3
+
--- setup_scripts/install_vim_plugins.zsh
+++ setup_scripts/install_vim_plugins.zsh
@@ -3,16 +3,9 @@
 source ~/.zsh_profile
 sudo apt install build-essential python3-dev
 
-# install Vundle plugin manager
-# git clone https://github.com/VundleVim/Vundle.vim.git ./nvim/.config/nvim/bundle/Vundle.vim
-
-#pushd $HOME/.vim/bundle/YouCompleteMe
-#python3 install.py --clangd-completer
-#popd 
-
 # install vim-plug
-curl -fLo ~/.dotfiles/nvim/.config/nvim/site/autoload/plug.vim --create-dirs \
-   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sh -c "curl -fLo $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 
 #for copilot we need to install node js 12 or superior
 curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
