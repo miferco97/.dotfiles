a7451dda62cf395938baa3603145a600c2f383fa
miferco97 <miferco97@gmail.com>
2022-02-12 09:02:13 +0100

    folder reestructured

---
+++ plugin_setup.zsh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+
+# installing vim plugins
+zsh setup_scripts/install_vim_plugins.zsh
+
+#setup_symlinks
+zsh setup_scripts/setup_symlinks.zsh
+
--- setup_scripts/install_vim_plugins.zsh
+++ setup_scripts/install_vim_plugins.zsh
@@ -1,20 +1,22 @@
-#!/usr/bin/env zsh
+#!/bin/zsh
 
+source ~/.zsh_profile
 sudo apt install build-essential python3-dev
 
 # install Vundle plugin manager
 # git clone https://github.com/VundleVim/Vundle.vim.git ./nvim/.config/nvim/bundle/Vundle.vim
 
-# install vim-plug
-curl -fLo ~/.var/app/io.neovim.nvim/data/nvim/site/autoload/plug.vim --create-dirs \
-   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-nvim +'PlugInstall --sync' +qa
-
 #pushd $HOME/.vim/bundle/YouCompleteMe
 #python3 install.py --clangd-completer
 #popd 
 
+# install vim-plug
+curl -fLo ~/.var/app/io.neovim.nvim/data/nvim/site/autoload/plug.vim --create-dirs \
+   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 #for copilot we need to install node js 12 or superior
 curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
+
+# install pulgins in nvim
+nvim +'PlugInstall --sync' +qa
