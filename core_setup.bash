351062a8c09c99c19339ca79f308c20d8ebc81d1
miferco97 <miferco97@gmail.com>
2022-02-12 21:34:50 +0100

    nvim into .zshrc

--- core_setup.bash
+++ core_setup.bash
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+#install curl and tmux
+sudo apt install curl tmux -y
 # install zsh
 sudo apt install zsh -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+zsh -c 'print zsh installed correctly :D; zsh -i' && \
 sudo apt-get install fonts-powerline
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
